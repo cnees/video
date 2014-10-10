4aa87a30319ccb0ade687c49bfb780d8bcf48b34
Clara Nees <cnees@umich.edu>
2014-10-10 10:03:41 -0400

    Deleting a reply decrements the parenent's reply count

--- delete.php
+++ delete.php
@@ -8,5 +8,13 @@ $LTI = \Tsugi\Core\LTIX::requireData(array('user_id'));
 if(!isset($_POST['delete'])) {
 	die();
 }
+$parent = $PDOX->rowDie("SELECT parent FROM {$CFG->dbprefix}video_comments WHERE user_id=:UID AND id=:ID LIMIT 1", array(":UID" => $USER->id, ":ID" => $_POST['delete']));
+if(isset($parent['parent'])) {
+	$PDOX->queryDie("UPDATE {$CFG->dbprefix}video_comments
+		SET replies = replies - 1
+		WHERE id=:ID
+		LIMIT 1",
+		array(":ID" => $parent['parent']));	
+}
 $PDOX->queryDie("DELETE FROM {$CFG->dbprefix}video_comments WHERE user_id=:UID AND id=:ID LIMIT 1", array(":UID" => $USER->id, ":ID" => $_POST['delete']));
 ?>
